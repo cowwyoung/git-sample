e0cb50459cbc4371b31cfd645917a025154ea622
lsy <developer17.lsy@gmail.com>
2023-03-15 16:45:31 +0900

    blk-sampleDB-commit

---
+++ blk-sampleDB.sql
@@ -0,0 +1,8 @@
+/*
+-- Query: SELECT * FROM market_db.buy
+LIMIT 0, 1000
+
+-- Date: 2023-03-15 16:43
+*/
+INSERT INTO `` (`num`,`mem_id`,`prod_name`,`group_name`,`price`,`amount`) VALUES (4,'BLK','지갑',NULL,30,'2');
+INSERT INTO `` (`num`,`mem_id`,`prod_name`,`group_name`,`price`,`amount`) VALUES (5,'BLK','맥북프로','디지털',1000,'1');
